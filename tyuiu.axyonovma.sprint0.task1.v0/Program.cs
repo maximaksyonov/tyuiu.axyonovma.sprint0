f2ba7e9085b83aedc64d2a523aba6e30337456da
Axyonov Maxim <tyuiu.axyonovma@outlook.com>
2025-09-10 22:13:13 +0500

    Task1

---
+++ tyuiu.axyonovma.sprint0.task1.v0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.axyonovma.sprint0.task1.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Hello World");
+            string name = "Максим";
+            int age = 17;
+            Console.WriteLine("Здравствуйте меня зовут "+name+" мне "+age+" лет");
+            Console.ReadKey ();
+        }
+    }
+}
