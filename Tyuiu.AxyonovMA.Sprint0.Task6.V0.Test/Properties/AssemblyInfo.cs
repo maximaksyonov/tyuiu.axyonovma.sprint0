113a98e0724c2d17666307b02fef3951f550103a
Axyonov Maxim <tyuiu.axyonovma@outlook.com>
2025-09-14 15:00:04 +0500

    Task 6

---
+++ Tyuiu.AxyonovMA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AxyonovMA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AxyonovMA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56c5ceef-3d3b-45e0-a3e6-929a92f86c08")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.AxyonovMA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AxyonovMA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.AxyonovMA.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива: " + Dataservice.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива: " + Dataservice.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива: " + Dataservice.MultiplicationArray(arraynums));
+
+            Console.ReadKey();
+        }
+    }
+}
